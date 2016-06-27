54d1daf643810c46df4f786d40e2885dcc0fcef9
easter42 <cbrunet@student.42.fr>
2016-06-27 15:25:10 +0200

    Search and return ok, availables in the logs. Now we will see the display

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var github = require('octonode');
+
 var app = express();
+var client = github.client();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -9,13 +12,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.get('/', function (req, res) {
   res.render('index', { title: 'Who\'s following you?' });
 });
 
 app.post('/', function (req, res) {
   var username = req.body['username'];
-  res.render('index', { title: 'Who\'s following you?', user: username });
+  var ghuser = client.user(username);
+
+  ghuser.followers({}, function(err, data, headers) {
+    if (!data)
+      res.render('index', { title: 'Who\'s following you?', user: username });
+    else {
+      var users = [];
+      data.forEach(function(user) {
+        users.push({
+          login: user['login'],
+          avatar_url: user['avatar_url'],
+          url: user['url']
+        });
+      });
+      console.log(users);
+      res.render('index', { title: 'Who\'s following you?', user: username, followers: users });
+    }
+  });
 });
 
 app.listen(3000, function () {
